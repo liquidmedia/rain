04eea9f31e249d4d5594ad248bcbffca02c27008
Jeremie Wood <jeremie@jeremiewood.com>
2012-06-20 12:52:36 -0400

    Don't precompile CKEditor

--- lib/rain.rb
+++ lib/rain.rb
@@ -7,10 +7,10 @@ module Rain
   end
 
   def self.assets
-    asset_list = Dir[root_path.join('vendor/assets/javascripts/ckeditor/**', '*.{js,css}')].inject([]) do |list, path|
-      list << Pathname.new(path).relative_path_from(root_path.join('vendor/assets/javascripts'))
-    end
-    asset_list << Pathname('rain.js')
+    #asset_list = Dir[root_path.join('vendor/assets/javascripts/ckeditor/**', '*.{js,css}')].inject([]) do |list, path|
+    #  list << Pathname.new(path).relative_path_from(root_path.join('vendor/assets/javascripts'))
+    #end
+    [Pathname('rain.js')]
   end
 
 end
